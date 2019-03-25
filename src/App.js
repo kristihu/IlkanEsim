a66c0b6f1dd5b86f2c35e0088c1817ce65a202a9
kristihu <44356817+kristihu@users.noreply.github.com>
2019-03-25 22:11:59 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {getAllMedia} from './utils/MediaAPI';
+import Nav from './components/nav'
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Single from './views/Single';
+
+
+class App extends Component {
+
+  state = {
+    picArray: [],
+  };
+
+  componentDidMount() {
+   getAllMedia().then(pics => {
+     this.setState({picArray: pics});
+   })
+  }
+
+  render() {
+    return (
+        <Router>
+        <div className="container">
+          <Nav/>
+          <Route exact path="/" render={(props)=>(
+              <Home {...props} picArray={this.state.picArray}/>
+          )}/>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/single/" component={Single}/>
+
+        </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
